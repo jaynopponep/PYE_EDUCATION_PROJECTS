4acaa701fdb412d39bf812a912da5a6e00c22ea1
jaynopponep <noppone.p@gmail.com>
2024-03-22 20:58:54 -0400

    week 2 projects

--- week2flask/week2project/app.py
+++ week2flask/week2project/app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 API_KEY = '4aabafc87014ff23f087fc1c2776cf55'
 
 
+def get_weather(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=imperial'
+    response = requests.get(url)
+    weather_data = response.json()
+
+    if weather_data.get('cod') != 200:
+        return None, 'Error: Unable to fetch weather data for ' + city
+
+    temperature = weather_data['main']['temp']
+    weather_description = weather_data['weather'][0]['description']
+    return f'{temperature} F, with {weather_description}'
+
+
 @app.route('/')
 def home():
     return render_template('form.html')
@@ -13,20 +26,14 @@ def home():
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
-    city = request.form['city']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-    response = requests.get(url)
-    weather_data = response.json()
+    city1 = request.form['city1']
+    city2 = request.form['city2']
 
-    if weather_data.get('cod') != 200:
-        return 'Error: Unable to fetch weather data'
+    weather1 = get_weather(city1)
+    weather2 = get_weather(city2)
 
-    temperature = weather_data['main']['temp']
-    weather_description = weather_data['weather'][0]['description']
-
-    return f'The weather in the city {city}: {temperature} C, with {weather_description}'
+    return f'The weather in {city1}: {weather1}<br>The weather in {city2}: {weather2}'
 
 
 if __name__ == '__main__':
     app.run()
-
