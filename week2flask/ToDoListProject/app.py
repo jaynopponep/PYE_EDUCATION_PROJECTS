e6a6281e846f91a2b794a6f75a5a7f86863294cf
jaynopponep <noppone.p@gmail.com>
2024-03-26 14:59:12 -0400

    Week 2

--- week2flask/ToDoListProject/app.py
+++ week2flask/ToDoListProject/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = []
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form['content']
+    tasks.append(task_content)
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    tasks.pop(task_id)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.run()
+
